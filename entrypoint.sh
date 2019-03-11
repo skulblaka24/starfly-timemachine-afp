8138536c6c3a0d80e1f9cb0dc241c345e56757f2
root <root@node1.starfly.ovh>
2019-03-11 23:02:30 +0100

    MAJ

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# USERNAME PASSWORD VOL_NAME VOL_ROOT [VOL_SIZE_MB]
+add-account gauth Skulblaka24 Time_Machine_Gauth /timemachine/gauth 2000000
+
 if [ ! -e /.initialized_user ] && [ ! -z "$AFP_LOGIN" ] && [ ! -z "$AFP_PASSWORD" ] && [ ! -z "$AFP_NAME" ] && [ ! -z $PUID ] && [ ! -z $PGID ]; then
     add-account -i $PUID -g $PGID "$AFP_LOGIN" "$AFP_PASSWORD" "$AFP_NAME" /timemachine $AFP_SIZE_LIMIT
     touch /.initialized_user
